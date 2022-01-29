40251686c83d0bd41f4f8f0a0bb4ca247cdc1f55
Vmbugua <vmbugua@cytonn.com>
2022-01-29 15:37:30 +0300

    welcome page changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,37 +29,22 @@
     @endif
 
     <div class="min-h-screen flex items-center justify-center">
-        <div class="flex flex-col justify-around h-full">
-            <div>
-                <h1 class="mb-6 text-gray-600 text-center font-light tracking-wider text-4xl sm:mb-8 sm:text-6xl">
-                    {{ config('app.name', 'Laravel') }}
-                </h1>
-                <ul class="flex flex-col space-y-2 sm:flex-row sm:flex-wrap sm:space-x-8 sm:space-y-0">
-                    <li>
-                        <a href="https://laravel.com/docs" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="Documentation">Documentation</a>
-                    </li>
-                    <li>
-                        <a href="https://laracasts.com" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="Laracasts">Laracasts</a>
-                    </li>
-                    <li>
-                        <a href="https://laravel-news.com" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="News">News</a>
-                    </li>
-                    <li>
-                        <a href="https://nova.laravel.com" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="Nova">Nova</a>
-                    </li>
-                    <li>
-                        <a href="https://forge.laravel.com" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="Forge">Forge</a>
-                    </li>
-                    <li>
-                        <a href="https://vapor.laravel.com" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="Vapor">Vapor</a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/laravel/laravel" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="GitHub">GitHub</a>
-                    </li>
-                    <li>
-                        <a href="https://tailwindcss.com" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="Tailwind Css">Tailwind CSS</a>
-                    </li>
-                </ul>
+        <div class="flex justify-center align-center h-full">
+            <div class="bg-indigo-600 text-white rounded shadow-xl py-5 px-5 w-full lg:w-10/12 xl:w-3/4">
+                <div class="flex flex-wrap -mx-3 items-center justify-center align-center">
+                    <h3 class="text-2xl">Welcome to Hot Tasks!</h3>
+                    <div class="w-1/4 px-3 text-center hidden md:block">
+                        <div class="p-5 xl:px-8 md:py-5">
+
+                        </div>
+                    </div>
+                    <div class="w-full sm:w-1/2 md:w-2/4 px-3 text-center">
+                        <div class="p-5 xl:px-8 md:py-5">
+
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
